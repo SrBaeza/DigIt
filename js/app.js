7fca8025f4309f72a30ebb231dbb679de9906b01
unknown <sr.generoso@gmail.com>
2019-12-15 01:55:53 +0100

    first logic

--- js/app.js
+++ js/app.js
@@ -1,6 +1,28 @@
-//variables globales
-var playerNrg = 100;
+/*variables globales*/
+
+//Referencia a la barra de energia en el codigo HTML
 var energyBar = document.getElementById("nrgBarID");
+//Valor Inicial de energia **no cambiar
+var initNrg = 100;
+//Valor actual de energia
+var currentNrg;
+//Valor de restauracion de energia
+var nrgValueIncrement = 5;
+
+//Referencia al contador de tiempo en el codigo HTML
+var timer = document.querySelector(".currentTime");
+//Referencia al intervalo del contador interno de tiempo
+var myInterval;
+//Valor inicial del contador de tiempo
+var initTime = 180;
+//Referencia al valor actual de contador
+var currentTime;
+//Valor de restauracion de tiempo
+var timeValueIncrement = 20;
+
+
+
+
 
 //Puesta en marcha del script
 document.body.onload = function() {
@@ -12,19 +34,43 @@ function StartGame() {
     ResetTimer();
     StartTimer();
     MakeTiles();
-    myNrgBar();
-}
+    resetcurrentNrg();
 
-
-//funcion para probar la barra de energia
-function myNrgBar() {
-    var nrgValueBasic = 5;
-    energyBar.style.width = (100 - nrgValueBasic) + "%";
-    energyBar.style.backgroundColor = "#F9BCCA";
 
 }
 
-//funcion para modificar la barra
+function resetcurrentNrg() {
+    currentNrg = initNrg;
+    energyBar.style.width = currentNrg + "%";
+    energyBar.style.backgroundColor = "hsl(" + (currentNrg - nrgValueIncrement) + ", 80%, 50%)";
+
+}
+
+//funcion para restaurar energia
+function nrgAdd() {
+
+    if (currentNrg >= 100) {
+        alert("maxhealth");
+    } else {
+        currentNrg = currentNrg + nrgValueIncrement;
+        energyBar.style.width = (currentNrg) + "%";
+        energyBar.style.backgroundColor = "hsl(" + currentNrg + ", 80%, 50%)";
+
+    }
+}
+
+//funcion para consumir energia
+function nrgSubstract() {
+    currentNrg = currentNrg - nrgValueIncrement;
+    if (currentNrg <= 0) {
+        alert("youloose");
+    } else {
+        energyBar.style.width = (currentNrg) + "%";
+        energyBar.style.backgroundColor = "hsl(" + currentNrg + ", 80%, 50%)";
+    }
+}
+
+
 
 //Funcion para crear elementos basados en un array
 function MakeTiles() {
@@ -37,14 +83,22 @@ function MakeTiles() {
         //Loop eje Y
         for (var y = 0; y < 13; y++) {
 
+
             //crear item de lista
             var item = document.createElement('div');
 
             // //añadir clase al item
-            item.classList.add("tile");
+            item.classList.add("tile", "undigged");
 
-            // //establecer contenido del div
-            item.appendChild(document.createTextNode("x"));
+            //Test añadir power
+            if (y % 2 == 0) {
+                item.classList.add("powerF");
+            } else {
+                item.classList.add("powerT");
+            }
+            //añadimos eventos del raton
+            item.addEventListener("click", this.tileClick);
+
 
             //añadir el item a la lista
             list.appendChild(item);
@@ -52,33 +106,119 @@ function MakeTiles() {
     }
 }
 
-// Funciones de temporizador del juego
-var second = 0,
-    minute = 0;
-var timer = document.querySelector(".currentTime");
-var myInterval;
+function PlacePowerUps(x, y) {
+    var test = "powerF";
+    switch (test) {
+        case "powerF":
+
+            break;
+        case "powerT":
+            break;
+        case "item":
+            break;
+        default:
+            break;
+    };
+}
+
+//Funciones de click on tile
+function tileClick() {
+    //le quitamos el eventlistener para no poder hacerle click
+    this.removeEventListener("click", tileClick);
+    //damos la vuelta a la tile
+    tileShow(this);
+    //comprobamos la accion de la tile
+    tileCheck(this);
+}
+//funcion par mostrar Tile
+function tileShow(currentElement) {
+    currentElement.classList.remove("undigged");
+    currentElement.classList.add("digged");
+}
+
+function tileCheck(currentElement) {
+    //comprobamos si la tile es de tipo powerFuel
+    if (currentElement.classList.contains("powerF")) {
+        /*añadimos la imagen*/
+        //creamos el elemento de la imagen       
+        var newImg = document.createElement('img');
+        //añadimos clase a la imagen
+        newImg.classList.add("pwrItemImg");
+        //añadimos los parametros de ruta la imagen
+        newImg.src = 'img/power_fuel_full.png';
+        //finalmente añadimos el div a la tile
+        currentElement.appendChild(newImg);
+
+        /*ejecutamos la funcion de añadir energia*/
+        nrgAdd();
+    } else {
+        //Si no es un item de energia
+        //ejecutamos la funcion de reducir energia
+        nrgSubstract();
+    }
+
+    //comprobamos si la tile es de tipo PowerTime
+    if (currentElement.classList.contains("powerT")) {
+        /*añadimos la imagen*/
+        //creamos el elemento de la imagen       
+        var newImg = document.createElement('img');
+        //añadimos clase a la imagen
+        newImg.classList.add("pwrItemImg");
+        //añadimos los parametros de ruta la imagen
+        newImg.src = 'img/power_timer_full.png';
+        //finalmente añadimos el div a la tile
+        currentElement.appendChild(newImg);
+
+        /*Ejecutamos la funcion para añadir tiempo*/
+        timeAdd();
+    }
+
+    //Comprobamos si la tile es de tipo Item
+    if (currentElement.classList.contains("item")) {
+        console.log("it has class item");
+    }
+
+    //Comprobamos si la tile es de tipo normal
+    if (currentElement.classList.contains("tile")) {
+        console.log("it has class tile");
+    }
+}
+
+
+
+/* Funciones de temporizador del juego*/
+
 
 // Iniciar temporizador
 function StartTimer() {
     myInterval = setInterval(
         function() {
-            timer.innerHTML = minute + " mins " + second + " secs";
-            second++;
-            CheckTimer();
-            if (second == 60) {
-                minute++;
-                second = 0;
-            }
+            //Mostrar el valor en pantalla
+            timer.innerHTML = (Math.trunc(currentTime / 60)) + " mins " + (currentTime % 60) + " secs";
+            //restamos un segundo
+            currentTime--;
+            //comprobamos si se ha agotado el tiempo
+            if (currentTime <= 0) { window.alert("Te has quedado sin tiempo"); }
+            //Intervalo sobre el cual se va a relanzar la funcion en milisegundos
         }, 1000);
 }
 // Resetear Temporizador
 function ResetTimer() {
-    timer.innerHTML = "0 mins 0 secs";
+    //reseteamos el contador
     clearInterval(myInterval);
+    //resetear valor total de tiempo de juego
+    currentTime = initTime;
+    //primer valor mostrado en pantalla
+    timer.innerHTML = (Math.trunc(currentTime / 60)) + " mins " + (currentTime % 60) + " secs";
 }
-//Comprobacion de tiempo limite *falta poner funcion de final de juego.
-function CheckTimer() {
-    if (minute >= 5) {
-        window.alert("Tiempo sobrepasado");
+
+//Funcion para añadir tiempo al contador
+function timeAdd() {
+    //Comprobamos si al añadir tiempo nos saldriamos del maximo
+    if (currentTime >= initTime - timeValueIncrement) {
+        window.alert("Tiempo maximo alcanzado");
+    } else {
+        //añadimos tiempo
+        currentTime = currentTime + timeValueIncrement;
     }
 }
